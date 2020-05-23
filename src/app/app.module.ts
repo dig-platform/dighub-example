6d1753dfd21f50956836d7496e295b8d693f26b7
forrest lyman <forrestlyman@gmail.com>
2020-05-22 17:25:39 -0700

    adds components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
